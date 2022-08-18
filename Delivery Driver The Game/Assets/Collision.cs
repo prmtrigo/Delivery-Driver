dadad59eb02f724c01ca2f9f8c386cc9c2218cf7
Paulo <trigopaulo@gmail.com>
2022-08-18 18:39:31 -0300

    collision ouch

--- Delivery Driver The Game/Assets/Collision.cs
+++ Delivery Driver The Game/Assets/Collision.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnCollisionEnter2D(Collision2D other) 
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Ai!");
     }
 }
